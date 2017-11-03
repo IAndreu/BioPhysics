12dffc600172cab662419b4f02cd6f63c541f848
Josep Ll. Gelpi <gelpi@ub.edu>
2017-11-03 12:47:19 +0100

    Updated final code

--- polarcontacts.py
+++ polarcontacts.py
@@ -10,6 +10,8 @@ from Bio.PDB.NeighborSearch import NeighborSearch
 from Bio.PDB.PDBParser import PDBParser
 
 import StructureWrapper
+import ForceField
+import ResLib
 
 import os
 import sys
@@ -39,14 +41,30 @@ def main():
         dest='backonly',
         help='Restrict to backbone'
     )
-
     parser.add_argument(
         '--nowats', 
         action='store_true', 
         dest='nowats',
         help='Exclude water molecules'
     )
-
+    parser.add_argument(
+        '--diel', 
+        type= float,
+        action='store', 
+        dest='diel',
+        default = 1.0,
+        help='Relative dielectric constant'
+    )
+    parser.add_argument(
+        '--vdw',
+        dest='vdwprm',
+        help='Vdw Parameters file'
+    )
+    parser.add_argument(
+        '--rlib',
+        dest='reslib', 
+        help='Residue library'
+    )
     parser.add_argument('pdb_path')
     
     args = parser.parse_args()
@@ -59,7 +77,19 @@ def main():
     backonly = args.backonly
     nowats =args.nowats
     pdb_path = args.pdb_path
+    vdwprm = args.vdwprm
+    reslib = args.reslib
+    diel = args.diel
     
+
+    
+    #Load Vdw Parameters
+    ff = ForceField.VdwParamset(vdwprm)
+    print (ff.ntypes,'atom types loaded')
+    #Load res Library
+    rl = ResLib.ResiduesDataLib(reslib)
+    print (rl.nres,'residue types loaded')
+
     if not pdb_path:
         parser.print_help()
         sys.exit(2)        
@@ -95,20 +125,30 @@ def main():
     for at1, at2 in nbsearch.search_all(HBLNK):
         if at1.get_parent() == at2.get_parent():
             continue
+        if at1.get_serial_number() > at2.get_serial_number():
+            continue
  #Discard covalents and neighbours
         if (at1-at2) < COVLNK:
             continue
-        if abs(at2.get_parent().id[1] - at1.get_parent().id[1]) == 1:
+        if at2.get_parent().id[1] - at1.get_parent().id[1] == 1:
             continue
 # remove waters
         if nowats:
             if at1.get_parent().get_resname() in waternames \
                 or at2.get_parent().get_resname() in waternames:
                 continue
-        if at1.get_serial_number() < at2.get_serial_number():
-            hblist.append([StructureWrapper.Atom(at1,1),StructureWrapper.Atom(at2,1)])
-        else:
-            hblist.append([StructureWrapper.Atom(at2,1),StructureWrapper.Atom(at1,1)])
+        Atom1 = StructureWrapper.Atom(
+            at1, 1, 
+            rl.getParams(at1.get_parent().get_resname(),at1.id),
+            ff.atTypes
+        )
+        Atom2 = StructureWrapper.Atom(
+            at2,1,
+            rl.getParams(at2.get_parent().get_resname(),at2.id),
+            ff.atTypes
+        )
+
+        hblist.append([Atom1,Atom2])
 
     print ()
     print ("Polar contacts")
@@ -122,6 +162,29 @@ def main():
             hb[0].at - hb[1].at
             )
         )
+    print ()
+    print ("Residue interactions")
+    
+    # make list of Residue pairs
+    resList = []
+    for hb in hblist:
+        r1 = StructureWrapper.Residue(hb[0].at.get_parent(),1, ff, rl)
+        r2 = StructureWrapper.Residue(hb[1].at.get_parent(),1, ff, rl)
+        if [r1,r2] not in resList:
+            resList.append([r1,r2])
+    
+    for rpair in sorted(resList, key=lambda i: i[0].resNum()):
+        eint = rpair[0].electrInt(rpair[1],diel)
+        evdw = rpair[0].vdwInt(rpair[1])
+        print (
+            '{:10} {:10} {: 8.4f} {: 8.4f} {: 8.4f}'.format(
+                rpair[0].resid(), 
+                rpair[1].resid(),
+                eint,
+                evdw,
+                eint+evdw)
+        )
+
     
 if __name__ == "__main__":
     main()
